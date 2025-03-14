a94d7dec6c8a61e35a74cd47c0c32d052a60b672
Christian Breitmos <112418964+Christian-Breitmos@users.noreply.github.com>
2025-03-14 01:11:41 -0400

    Created a new task component to be able to add new task to the list of task a person has, added a function to be able to complete a task and the task goes away from view. Add a small feature when clicking on a user they stay highlighted like when you hover over them.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { type User } from './user.model';
 // type User = {
 //   id: string;
 //   avatar: string;
 //   name: string;
 // };
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
 
 @Component({
   selector: 'app-user',
@@ -20,6 +15,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
